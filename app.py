5dbfc8342dd37ffe8210b8169208d87e6c6c10ee
beccap05 <117544795+beccap05@users.noreply.github.com>
2022-11-05 22:03:04 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import render_template, flash, redirect, url_for, Flask, request
+#from app import app
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+@app.route('/')
+def index():
+    return render_template('layout.html')
+
+@app.route("/about", methods=["GET", "POST"])
+def about():
+    if request.method == "POST" or request.method == "GET":
+        return render_template("about.html")
