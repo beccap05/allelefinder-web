6d9b4cc5664f8642d8966ad79aa30628f37a759b
beccap05 <117544795+beccap05@users.noreply.github.com>
2022-11-05 22:03:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
 @app.route('/')
 def index():
-    return render_template('layout.html')
+    return render_template('index.html')
 
 @app.route("/about", methods=["GET", "POST"])
 def about():
